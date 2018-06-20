ad3e93e3a7c91ce712d48029020a75c9dde1f76f
sdlm <sdlmerr@yandex.ru>
2018-06-20 18:35:21 +0300

    packaging like pip package

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='marshmallow_orm_drivers',
+      version='0.1',
+      description='ORM drivers for marshmallow library.',
+      url='https://github.com/sdlm/marshmallow_orm_drivers.git',
+      author='Arseniy Kuznetsov',
+      author_email='sdlmerr@yandex.ru',
+      license='MIT',
+      packages=['marshmallow_orm_drivers'],
+      zip_safe=False)
