d8d404259084db8fe2652ec2498468c03778121f
sdlm <sdlmerr@yandex.ru>
2018-06-22 06:42:02 +0300

    minor

--- marshmallow_orm_drivers/tests/tests.py
+++ marshmallow_orm_drivers/tests/tests.py
@@ -1,1 +1,17 @@
 # todo: write a lot of tests for each ORM!
+
+# interface for DB
+
+# basic test
+
+# test fk
+
+# test reverse fk
+
+# test m2m
+
+# test reverse m2m
+
+# test unique
+
+# test several unique
